d08da173de2391ce5f979e7e6e47159f51546301
Howard <sgusev6435@gmail.com>
2023-04-16 01:14:28 +0300

    Rewrite createStore to configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,10 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore( 
-                combineReducers({heroes, filters}),
-                compose(
-                    applyMiddleware(ReduxThunk), 
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                )
-);
+const store = configureStore({
+    reducer: {heroes, filters},
+    devTools: process.env.NODE_ENV !== 'production'
+});
 
 export default store;
